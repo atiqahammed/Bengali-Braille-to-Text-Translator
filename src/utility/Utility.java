1770f07ac4f461fe6c6ba05a98fa6caf0705f683
atiqahammed <atiqahammedshamim@gmail.com>
2019-02-14 12:45:42 +0600

    application height and weidth is added

--- src/utility/Utility.java
+++ src/utility/Utility.java
@@ -5,5 +5,7 @@ import java.io.File;
 public class Utility {
 	//public static File choosedFile;
 	public static String BRAILLE_TO_TEXT_TRANSLATOR = "Braille to Text Translator";
+	public static int APPLICATION_HEIGHT = 500;
+	public static int APPLICATION_WIDTH = 840;
 
 }
