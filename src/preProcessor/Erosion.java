d9143bc18182ec5ef748564360cad6da8748f7e1
Atiq Ahammed <30492609+atiqahammed@users.noreply.github.com>
2019-03-10 16:44:46 +0600

    need to check previous code

--- src/preProcessor/Erosion.java
+++ src/preProcessor/Erosion.java
@@ -7,84 +7,81 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class Erosion extends ImageProcessor{
+public class Erosion {
 
 
 	private int row[] = {-1, 0, 1, 0};
 	private int col[] = {0, 1, 0, -1};
-	protected BufferedImage image2;
-	int total = 0;
+
+
+	public File getImage(File imageFile) {
+
+		//File testImage = new File("input/test.jpg");
+		BufferedImage testImg = null;
+
+		try {
+			testImg = ImageIO.read(imageFile);
+
+		} catch (IOException e) {
+			System.out.println("no input file");
+
+		}
+
+
+		int widthTest = testImg.getWidth();
+		int heightTest = testImg.getHeight();
+
+		Color myWhite = new Color(255, 255, 255); // Color white
+		int rgb = myWhite.getRGB();
+
+		BufferedImage theImage = new BufferedImage(widthTest, heightTest, BufferedImage.TYPE_INT_RGB);
 
 
 
-	@Override
-	protected void writePixel(int x, int y) {
+		int blackCount = 0;
 
-		//System.out.println(height * width);
-		Color c = new Color(image.getRGB(y, x));
-		int grayScale = (int) c.getRed();
+		for (int i = 0; i < widthTest; i++) {
+			for (int j = 0; j < heightTest; j++) {
+				Color c1 = new Color(testImg.getRGB(i, j));
+				int grayValue = (int) c1.getRed();
+				int count = 0;
 
-		/*if(grayScale == 0) {
-			System.out.println("paichi");
-			total++;
-			System.out.println("-- " + total);
-		}*/
+				for(int index = 0; index<4; index++) {
 
-		int count = 0;
+					int tempX = j + row[i] ;
+					int tempY = i + col[i];
+					if(tempX>= 0 && tempX < heightTest && tempY >= 0 && tempY < widthTest) {
+						//System.out.println("in");
+						Color color = new Color(testImg.getRGB(tempY, tempX));
+						int neighbourValue = (int) color.getRed();
+						//System.out.println(grayValue);
+						if(neighbourValue == 0) {
+							count++;
+							//System.out.println(i + " " + count);
+						}
+					}
 
-
-		for(int i = 0; i < 4; i++) {
-			int tempX = row[i];
-			int tempY = col[i];
-			if(tempX>= 0 && tempX < height && tempY >= 0 && tempY < width) {
-				//System.out.println("in");
-				Color color = new Color(image.getRGB(tempY, tempX));
-				int grayValue = (int) color.getRed();
-				//System.out.println(grayValue);
-				if(grayValue == 0) {
-					count++;
-					System.out.println(i + " " + count);
 				}
+
+				if(count == 4)
+					theImage.setRGB(i, j, 0);
+				else
+					theImage.setRGB(i, j, rgb);
+
+
+
+				//if(grayValue == 0)
+				//	blackCount++;
 			}
 
 		}
 
+		System.out.println(blackCount);
 
 
-		Color color;
-		if (count == 4) {
-			color = new Color(0, 0,  0);
-		}
 
-		else {
-			color = new Color(255, 255, 255);
-		}
-
-		//image2.setRGB(y, x, color.getRGB());
+		return null;
 	}
 
-	@Override
-	protected void initializedPreOperation() {
-
-		imageFileName = "EROSION_Image_" + imageFileName;
-		image2 = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-
-	}
-
-	public File getImage(File imageFile) {
-		return convert(imageFile);
-	}
-
-
-	protected File getImageFile() {
-		imagePath = imageFileName + "." + imageType;
-		File ouptut = new File(imagePath);
-		try {
-			ImageIO.write(image2, imageType, ouptut);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return ouptut;
-	}
 
 }
