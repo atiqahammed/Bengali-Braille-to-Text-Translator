d361f6bbf2c03753124dc92687dce12863bac8e8
atiqahammed <atiqahammedshamim@gmail.com>
2019-07-01 14:50:40 +0600

    button is now accessable

--- src/main/Controler.java
+++ src/main/Controler.java
@@ -14,67 +14,70 @@ import javafx.scene.layout.BorderPane;
 public class Controler implements Initializable{
 
 	@FXML
-	private BorderPane borderpane;
+	private BorderPane brailleborderpane;
 
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 
+		System.out.println("init function");
 	}
 
 	@FXML
-	private void btn1(MouseEvent mouseEvent) {
-//		loadUI("u1");
-		System.out.println("ok");
-
-		Parent root = null;
-		try {
-			root = FXMLLoader.load(getClass().getResource("u1.fxml"));
-		} catch (IOException e) {
-
-			System.out.println("error");
-		}
-
-		borderpane.setCenter(root);
-		if(root == null) {
-			System.out.println("r");
-		}
-
-		if (borderpane == null) {
-			System.out.println("b");
-		}
-
-	}
-
-	@FXML
-	private void btn2(MouseEvent mouseEvent) {
-		loadUI("u2");
-	}
-
-	@FXML
-	private void btn3(MouseEvent mouseEvent) {
-		loadUI("u3");
-	}
-
-	@FXML
-	private void btn4(MouseEvent mouseEvent) {
-		loadUI("u4");
-	}
-
-	private void loadUI(String ui) {
-
-		Parent root = null;
-		try {
-			root = FXMLLoader.load(getClass().getResource(ui + ".fxml"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		borderpane.setCenter(root);
+	private void main_ui_home_button(MouseEvent mouseEvent) {
+////		loadUI("u1");
+//		System.out.println("ok");
+//
+//		Parent root = null;
+//		try {
+//			root = FXMLLoader.load(getClass().getResource("u1.fxml"));
+//		} catch (IOException e) {
+//
+//			System.out.println("error");
+//		}
+//
+//		main_border_pane.setCenter(root);
+//		if(root == null) {
+//			System.out.println("r");
+//		}
+//
+//		if (main_border_pane == null) {
+//			System.out.println("b");
+//		}
+		
+		System.out.println("home button");
 
 	}
+
+//	@FXML
+//	private void btn2(MouseEvent mouseEvent) {
+//		loadUI("u2");
+//	}
+//
+//	@FXML
+//	private void btn3(MouseEvent mouseEvent) {
+//		loadUI("u3");
+//	}
+//
+//	@FXML
+//	private void btn4(MouseEvent mouseEvent) {
+//		loadUI("u4");
+//	}
+
+//	private void loadUI(String ui) {
+//
+//		Parent root = null;
+//		try {
+//			root = FXMLLoader.load(getClass().getResource(ui + ".fxml"));
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//		main_border_pane.setCenter(root);
+//
+//	}
 
 }
 
--- src/main/TempTest.java
+++ src/main/TempTest.java
@@ -1,15 +1,29 @@
 package main;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import util.Utils;
 
-public class TempTest {
+public class TempTest extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		Parent root = FXMLLoader.load(getClass().getResource("test_home_ui.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle(Utils.BRAILLE_TO_TEXT_TRANSLATOR);
+		primaryStage.show();
+
+
+
+	}
 
 	public static void main(String[] args) {
-
-//		System.out.println(Utils.LETTERS.getLetters("110000"));
-
-		
-
+		launch(args);
 	}
 
 }
