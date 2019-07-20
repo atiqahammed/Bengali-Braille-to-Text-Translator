a16dd1cc8f0d241034dbc4ea9fa1e7e3254c3f49
Atiq Ahammed <30492609+atiqahammed@users.noreply.github.com>
2019-07-20 16:28:59 +0600

    Remove the TestMain file as it was an unnecessary file for this project

--- src/userInterface/TestMain.java
+++
@@ -1,9 +0,0 @@
-package userInterface;
-
-import javax.swing.JOptionPane;
-
-public class TestMain {
-	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Hello world");
-	}
-}
