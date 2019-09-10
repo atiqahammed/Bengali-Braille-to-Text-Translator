ae8e292eb3adaa2287a2525509e66be95fabfff7
Atiq Ahammed <30492609+atiqahammed@users.noreply.github.com>
2019-09-10 21:12:37 +0600

    Home About Ui and button is ready

---
+++ src/BBTT/App.java
@@ -0,0 +1,31 @@
+package BBTT;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import util.Utils;
+
+public class App extends Application{
+
+	public static Stage STAGE;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		Parent root = FXMLLoader.load(getClass().getResource("test_home_ui.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle(Utils.BRAILLE_TO_TEXT_TRANSLATOR);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+		STAGE = primaryStage;
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
